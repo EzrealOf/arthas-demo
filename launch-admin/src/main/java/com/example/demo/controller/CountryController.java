9cf95bc6d0bae8bc7c636185a3f235a21e7c7c73
Ezrealok <ezrealok@163.com>
2021-12-08 14:02:12 +0800

    fix:错误的返回

--- launch-admin/src/main/java/com/example/demo/controller/CountryController.java
+++ launch-admin/src/main/java/com/example/demo/controller/CountryController.java
@@ -4,13 +4,11 @@ import com.example.demo.model.FarmConfig;
 import com.example.demo.service.CountryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
-@Controller
+@RestController
 public class CountryController {
 
     private static final Logger logger = LoggerFactory.getLogger(CountryController.class);
@@ -20,7 +18,8 @@ public class CountryController {
 
 
     @GetMapping("/initFarm")
-    public String initFarm(@RequestParam("name") String name, @RequestParam("type")String type) {
+    @ResponseBody
+    public String initFarm(@RequestParam("name") String name, @RequestParam("type") String type) {
         FarmConfig config = FarmConfig.builder().name(name).type(type).build();
         countryService.start(config);
         return "success";
